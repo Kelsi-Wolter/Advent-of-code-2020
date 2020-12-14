8a8a8c43a4b8136e15b7a113fe6b66991c91fff5
Kelsi-Wolter <kelsi.wolter@gmail.com>
2020-12-14 20:33:40 +0000

    Found answer for part one, made very slow solution to part 2 --Day 1

--- day1.py
+++ day1.py
@@ -1,7 +1,10 @@
-expenses = open('expenses.txt')
+expenses = open('expenses.txt').read().split('\n')
 
-expense_report = expenses.read()
+expense_report = []
 
+
+for line in expenses:
+    expense_report.append(line)
 
 # Look at expense report, find two numbers that add up to 2020 and multiply them together, result the result
 
@@ -16,8 +19,22 @@ expense_report = expenses.read()
 
 report_set = set(expense_report)
 
+
+
 for expense in report_set:
-    remainder = 2020 - expense
+
+    remainder = str(2020 - int(expense))
     if remainder in report_set:
-        answer = remainder * expense
-        return answer, remainder, expense
+        answer = int(remainder) * int(expense)
+        print(f'Part one answer is {answer}, the two expenses are {remainder} and {expense}')
+
+
+for expense_1 in report_set:
+    for expense_2 in report_set:
+        for expense_3 in report_set:
+            sum = int(expense_1) + int(expense_2) + int(expense_3)
+            if sum == 2020:
+                answer = int(expense_1) * int(expense_2) * int(expense_3)
+                print(f'The answer for part 2 is: {answer}, the expenses were {expense_1}, {expense_2}, {expense_3}.')
+                break
+
