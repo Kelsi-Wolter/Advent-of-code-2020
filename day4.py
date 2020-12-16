55338711e41447fa1a445fbf711a826b7224a67d
vagrant <vagrant@vagrant.vm>
2020-12-16 03:14:58 +0000

    still getting wrong answer for part 1 day 4

--- day4.py
+++ day4.py
@@ -8,6 +8,36 @@
 # if yes, need to have byr, iyr, eyr, hgt, hcl, ecl, pid 
 #     --> valid
 
-text_file = open('passports.txt').read().split('\n')
+text_file = open('passports.txt').read().split('\n\n')
 
-print(text_file)
+passport_dict = {}
+all_passports = {}
+passport_list = []
+
+for passport in text_file:
+    passport = passport.replace('\n', ' ').split(' ')
+    passport_list.append(passport)
+
+n = 0
+for passport in passport_list:
+    for field in passport:
+        keys = field.split(':')
+        passport_dict[keys[0]] = keys[1]
+    
+    all_passports[str(n)] = passport_dict
+    n += 1
+    passport_dict = {}
+valid = 0
+for person in all_passports:
+    # if 'byr' and 'iyr' and 'eyr' and 'hgt' and 'hcl' and 'ecl' and 'pid' in all_passports[person]:
+    #     valid +=1
+
+    number_of_keys = len(all_passports[person])
+    
+    if number_of_keys == 8:
+        valid += 1
+    elif 'cid' not in passport_dict and number_of_keys == 7:
+        valid += 1
+        
+print(valid)
+
